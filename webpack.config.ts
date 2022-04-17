3b3b32df89b800eec60f60993b2ec7baf71daf8f
Fawad Ali <m.fawaadali98@gmail.com>
2022-04-17 23:33:24 +0500

    Set up webpack-dev-server

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,26 +1,31 @@
-import * as path from "path";
-import * as HtmlWebpackPlugin from "html-webpack-plugin";
-import { Configuration } from "webpack";
+import * as path from 'path';
+import * as HtmlWebpackPlugin from 'html-webpack-plugin';
+import { Configuration } from 'webpack';
 
 const config: Configuration = {
-  entry: "./src/index.ts",
+  mode: 'development',
+  entry: './src/index.ts',
   output: {
-    path: path.resolve(__dirname, "./dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        loader: "esbuild-loader",
+        loader: 'esbuild-loader',
         options: {
-          loader: "ts",
-          target: "es5",
+          loader: 'ts',
+          target: 'es5',
         },
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
 };
 
 export default config;

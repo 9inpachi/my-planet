7307f9e2199763fa58bd24df7c5fc134a9856e5d
Fawad Ali <m.fawaadali98@gmail.com>
2022-07-11 04:08:47 +0500

    Add logger

---
+++ src/common/utils/logger.ts
@@ -0,0 +1,24 @@
+export class Logger {
+  private static instance: Logger;
+  private prefix = 'LOGGER: ';
+
+  public getInstance(): Logger {
+    if (Logger.instance === undefined) {
+      Logger.instance = new Logger();
+    }
+
+    return Logger.instance;
+  }
+
+  public logInfo(message: string, ...additionalInfo: unknown[]) {
+    console.info(this.prefix, '[INFO]', message, ...additionalInfo);
+  }
+
+  public logError(message: string, ...additionalInfo: unknown[]) {
+    console.error(this.prefix, '[ERROR]', message, ...additionalInfo);
+  }
+
+  public logWarning(message: string, ...additionalInfo: unknown[]) {
+    console.warn(this.prefix, '[WARN]', message, ...additionalInfo);
+  }
+}

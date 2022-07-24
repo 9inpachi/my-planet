0242ed5b6ccb93281c2bad830f3fb0711c96d9ce
Fawad Ali <m.fawaadali98@gmail.com>
2022-07-25 00:31:22 +0500

    Add a simple object for wrapping existing objects

---
+++ src/my-planet/objects/simple-object.ts
@@ -0,0 +1,29 @@
+import { Mesh, Object3D } from 'three';
+import { BaseObject } from './base-object';
+import { BaseObjectProperties } from './library/base-object-properties';
+
+export type SimpleObjectProperties = {
+  object: Object3D;
+};
+
+export class SimpleObject extends BaseObject<SimpleObjectProperties> {
+  constructor(object: Object3D, properties?: BaseObjectProperties) {
+    super({ ...properties, object });
+  }
+
+  protected constructObject(): Object3D {
+    const { object, name, color } = this.properties;
+    if (name) {
+      object.name = name;
+    }
+    if (color) {
+      object.traverse((child) => {
+        if (child instanceof Mesh && 'color' in child.material) {
+          child.material.color = color;
+        }
+      });
+    }
+
+    return object;
+  }
+}

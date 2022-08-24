1167f05a2eb3ba31f126c262871767f4b5a32688
Fawad Ali <m.fawaadali98@gmail.com>
2022-08-24 21:04:37 +0500

    Allow specifying floor and split color in building object

--- src/my-planet/objects/building.ts
+++ src/my-planet/objects/building.ts
@@ -1,10 +1,18 @@
-import { BoxGeometry, Group, Mesh, MeshLambertMaterial } from 'three';
+import {
+  BoxGeometry,
+  ColorRepresentation,
+  Group,
+  Mesh,
+  MeshLambertMaterial,
+} from 'three';
 import { colors } from '../../common/lib/colors';
 import { BaseObject } from './base-object';
 
 export type BuildingProperties = {
   floors?: number;
   size?: number;
+  floorColor?: ColorRepresentation;
+  splitColor?: ColorRepresentation;
 };
 
 export class Building extends BaseObject<BuildingProperties> {
@@ -25,7 +33,9 @@ export class Building extends BaseObject<BuildingProperties> {
 
   private constructFloor(floor: number, size = 10) {
     const box = new BoxGeometry(size, size, size);
-    const material = new MeshLambertMaterial({ color: colors.building.floor });
+    const material = new MeshLambertMaterial({
+      color: this.properties?.floorColor ?? colors.building.floor,
+    });
     const mesh = new Mesh(box, material);
 
     const baseFloorHeight = size / 2;
@@ -38,7 +48,9 @@ export class Building extends BaseObject<BuildingProperties> {
   private constructSplit(floor: number, size = 10) {
     const height = this.getSplitHeight(size);
     const box = new BoxGeometry(size * 0.8, height, size * 0.8);
-    const material = new MeshLambertMaterial({ color: colors.building.split });
+    const material = new MeshLambertMaterial({
+      color: this.properties?.splitColor ?? colors.building.split,
+    });
     const mesh = new Mesh(box, material);
 
     const baseHeight = height / 2;

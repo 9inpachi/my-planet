cc1ea26bb0065b2664de024475df39b9a988177e
Fawad Ali <m.fawaadali98@gmail.com>
2022-10-11 23:18:33 +0500

    Set up building with vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,25 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   assetsInclude: ['**/*.gltf'],
   build: {
-    lib: {
-      entry: './src/index.ts',
-      formats: ['es'],
+    rollupOptions: {
+      output: {
+        assetFileNames: (asset) => {
+          const extension = asset.name?.split('.').pop() ?? '';
+          let directory: string;
+
+          if (/png|jpe?g|svg|gif/i.test(extension)) {
+            directory = 'images';
+          } else if (/gltf/i.test(extension)) {
+            directory = 'geometries';
+          } else if (/css/i.test(extension)) {
+            directory = 'css';
+          } else {
+            directory = '.';
+          }
+
+          return `assets/${directory}/[name][extname]`;
+        },
+      },
     },
   },
 });

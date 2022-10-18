4b44dcab37b7810caae02fa0802feb93aaf8020a
Fawad Ali <m.fawaadali98@gmail.com>
2022-10-18 13:12:53 +0500

    Add back icon to continent info

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,6 +3,7 @@ declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.gif';
+declare module '*.svg?raw';
 
 // Geometries
 declare module '*.gltf';
--- src/web/components/continent-info/continent-info.ts
+++ src/web/components/continent-info/continent-info.ts
@@ -1,10 +1,13 @@
 import { Component, template, styles, registerComponent } from '../component';
+import ArrowBackIcon from '../../../assets/icons/arrow-back.svg?raw';
 
 import continentTemplate from './continent-info.html?raw';
 import continentStyles from './continent-info.css?raw';
 
 @template(continentTemplate)
 @styles(continentStyles)
-class ContinentInfo extends Component {}
+class ContinentInfo extends Component {
+  private arrowBackIcon = ArrowBackIcon;
+}
 
 registerComponent(ContinentInfo);

c5519286d134ee72fcff8f8600a9044f92fea8aa
Fawad Ali <m.fawaadali98@gmail.com>
2022-11-02 18:41:23 +0500

    Increase favicon frames and optimize handling of favicons

--- src/web/components/planet-favicon/planet-favicon.ts
+++ src/web/components/planet-favicon/planet-favicon.ts
@@ -1,39 +1,28 @@
 import { Component, registerComponent } from '../component';
 
+import * as planetFavicons from './lib/icons';
+
 class PlanetFavicon extends Component {
-  private fraemDelay = 800;
-  private iconsCount = 7;
+  private frameDelay = 400;
 
   protected onInit() {
     this.animateFavicon();
   }
 
-  private async animateFavicon() {
+  private animateFavicon() {
     const link = document.head.querySelector(
       'link[rel="icon"]',
     ) as HTMLLinkElement;
-    const icons = await this.loadIcons();
-    let currentIconIndex = 1;
+    const faviconIcons = Object.values(planetFavicons);
+    let currentIconIndex = 0;
 
     setInterval(() => {
-      link.href = icons[currentIconIndex++];
+      link.href = faviconIcons[currentIconIndex++];
 
-      if (currentIconIndex > this.iconsCount) {
-        currentIconIndex = 1;
+      if (currentIconIndex === faviconIcons.length) {
+        currentIconIndex = 0;
       }
-    }, this.fraemDelay);
-  }
-
-  private async loadIcons(): Promise<string[]> {
-    const icons: string[] = [];
-
-    for (let i = 1; i <= this.iconsCount; i++) {
-      icons[i] = (
-        await import(`../../../assets/icons/favicon/planet-icon-${i}.ico`)
-      ).default;
-    }
-
-    return icons;
+    }, this.frameDelay);
   }
 }
 

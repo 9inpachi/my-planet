dfea0c127bc723e0e86667a3a2f0c06bdda912fd
Fawad Ali <m.fawaadali98@gmail.com>
2022-12-10 15:35:50 +0100

    Handle spacing between circle buttons inside the component itself

--- src/web/components/project/project.ts
+++ src/web/components/project/project.ts
@@ -1,10 +1,8 @@
-import { Component, registerComponent, styles, template } from '../component';
+import { Component, registerComponent, template } from '../component';
 
 import projectTemplate from './project.html?raw';
-import projectStyles from './project.css?raw';
 
 @template(projectTemplate)
-@styles(projectStyles)
 class Project extends Component {}
 
 registerComponent(Project);

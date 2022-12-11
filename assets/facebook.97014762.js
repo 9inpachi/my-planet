f555a120bd58710619959471eb345535b4d0b943
9inpachi <9inpachi@users.noreply.github.com>
2022-12-11 19:19:57 +0000

    Fix social icons not loading in built app 4207b4ef2cf0061f41a6377b7d83cf0a048eb1e2

---
+++ assets/facebook.97014762.js
@@ -0,0 +1,1 @@
+const e=""+new URL("images/facebook.svg",import.meta.url).href;export{e as default};
---
+++ assets/github.e46ff83e.js
@@ -0,0 +1,1 @@
+const t=""+new URL("images/github.svg",import.meta.url).href;export{t as default};

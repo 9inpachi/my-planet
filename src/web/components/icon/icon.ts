85fd32f74ae0940971bfe7fa3a1b994971700bda
Fawad Ali <m.fawaadali98@gmail.com>
2022-12-14 23:56:32 +0100

    Update icon component so it can load icons from assets and URL

--- src/web/components/icon/icon.ts
+++ src/web/components/icon/icon.ts
@@ -4,19 +4,53 @@ import iconStyles from './icon.css?raw';
 
 @styles(iconStyles)
 class Icon extends Component {
+  /** URL of the icon. */
   @property()
   src!: string;
+  /** Local icon located in `assets/icons`. */
+  @property()
+  icon!: string;
 
   protected onInit() {
     this.loadIcon();
   }
 
   private async loadIcon() {
-    const svg = await (await fetch(this.src)).text();
+    const svg = this.icon
+      ? await this.importIconSource()
+      : await this.fetchIconSource();
+
     this.shadowDOM.innerHTML = svg;
     // To be able to select the `svg` element with
     // `.icon-element::part(svg)` using css.
     this.shadowDOM.firstElementChild?.setAttribute('part', 'svg');
+  }
+
+  /**
+   * Import the icon from local assets.
+   */
+  private async importIconSource(): Promise<string> {
+    // Workaround. If the icon exists in a directory like "social", then
+    // we manually split the directory and icon. Because it's not
+    // possible to specify a path like `social/facebook` in `this.icon`.
+    // See https://github.com/rollup/plugins/tree/master/packages/dynamic-import-vars#limitations.
+    if (this.icon.includes('/')) {
+      const [directory, icon] = this.icon.split('/');
+
+      return (
+        await import(`../../../assets/icons/${directory}/${icon}.svg?raw`)
+      ).default;
+    } else {
+      return (await import(`../../../assets/icons/${this.icon}.svg?raw`))
+        .default;
+    }
+  }
+
+  /**
+   * Fetch the icon from a URL.
+   */
+  private async fetchIconSource(): Promise<string> {
+    return await (await fetch(this.src)).text();
   }
 }
 

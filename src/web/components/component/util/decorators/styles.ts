01a4ba1ae2897da0f77550bcb4d9d50bdc02970b
Fawad Ali <m.fawaadali98@gmail.com>
2022-12-18 19:51:53 +0100

    Make it possible to extend component styles from the parent component

--- src/web/components/component/util/decorators/styles.ts
+++ src/web/components/component/util/decorators/styles.ts
@@ -1,8 +1,15 @@
 export const styles = (styles: string): ClassDecorator => {
   return (target) => {
+    const allStyles = [styles];
+    // Inherit styles from the parent component (if the component is
+    // extended).
+    if (target.prototype.styles) {
+      allStyles.push(target.prototype.styles);
+    }
+
     // Sets `styles` as the property of the decorated class. God bless the prototype chain.
     Reflect.defineProperty(target.prototype, 'styles', {
-      value: styles,
+      value: allStyles.join('\n\n'),
     });
   };
 };

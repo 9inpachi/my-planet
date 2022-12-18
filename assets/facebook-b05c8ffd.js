b20b1609bee691c680945dce9b545910c680169c
9inpachi <9inpachi@users.noreply.github.com>
2022-12-18 17:27:57 +0000

    Add dev icons and prettify all svg files 5d7a18013c23b000d3b1419092498300eca76b49

---
+++ assets/facebook-b05c8ffd.js
@@ -0,0 +1,7 @@
+const r=`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">\r
+  <path\r
+    fill="currentColor"\r
+    d="M18.5,11.8V9.1c0-1.4,0-2.8,2-2.8h2V2.4c0-0.1-1.7-0.2-3.5-0.2c-3.7,0-5.9,2.3-5.9,6.5v3.2h-4v5.5h4v12.4h5.4 v-0.1V17.3h3.8l0.6-5.5H18.5z"\r
+  />\r
+</svg>\r
+`;export{r as default};

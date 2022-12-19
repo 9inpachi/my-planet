b0b1151ee6177e0fa7b2116b8f5ded9f4a50273d
Fawad Ali <m.fawaadali98@gmail.com>
2022-12-19 22:10:30 +0100

    Restyle heading element

--- src/web/components/heading/heading.ts
+++ src/web/components/heading/heading.ts
@@ -1,7 +1,18 @@
-import { Component, property, registerComponent } from '../component';
+import {
+  Component,
+  property,
+  registerComponent,
+  styles,
+  template,
+} from '../component';
+
+import headingTemplate from './heading.html?raw';
+import headingStyles from './heading.css?raw';
 
 type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
+@template(headingTemplate)
+@styles(headingStyles)
 class Heading extends Component {
   @property()
   level!: HeadingLevel;

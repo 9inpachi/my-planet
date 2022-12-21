e8b67312b449489305f87a4cff5f0205b1249e0d
Fawad Ali <m.fawaadali98@gmail.com>
2022-12-21 21:36:21 +0100

    Use heading component for headings

--- src/web/components/index.ts
+++ src/web/components/index.ts
@@ -1,7 +1,12 @@
+// Presentational Components
+
 import './heading/heading';
 import './arrow-button/arrow-button';
 import './icon/icon';
 import './circle-button/circle-button';
+import './timeline';
+
+// Application Components
 
 import './planet/planet';
 import './planet-favicon/planet-favicon';
@@ -10,5 +15,4 @@ import './planet-splash/planet-splash';
 import './continent-info';
 import './continent-pin/continent-pin';
 import './project/project';
-import './timeline';
 import './dev-icon/dev-icon';

c655d25a298ae23d2d3bd7b825920e210835f884
Fawad Ali <m.fawaadali98@gmail.com>
2023-02-12 19:30:14 +0100

    Working continents menu for accessibility

--- src/web/components/continents-menu/continents-menu.ts
+++ src/web/components/continents-menu/continents-menu.ts
@@ -1,10 +1,46 @@
-import { Component, registerComponent, template } from '../component';
+import { Component, registerComponent, styles, template } from '../component';
 
 import continentsMenuTemplate from './continents-menu.html?raw';
 import continentsMenuStyles from './continents-menu.css?raw';
 
 @template(continentsMenuTemplate)
-@template(continentsMenuStyles)
-class ContinentsMenu extends Component {}
+@styles(continentsMenuStyles)
+class ContinentsMenu extends Component {
+  private radius = 8;
+  private startAngle = 0;
+  private rotationAngle = 90;
+
+  protected onInit() {
+    const menuOptions = [
+      ...(this.getElement('continentsMenuOptions')?.children ?? []),
+    ] as HTMLElement[];
+
+    const menuOptionsCount = menuOptions.length - 1;
+    const singleAngle = this.startAngle + this.rotationAngle / menuOptionsCount;
+
+    let styles = '';
+
+    menuOptions.forEach((_menuOption, index) => {
+      styles += `.continents-menu-options.active > mp-circle-button:nth-child(${
+        index + 1
+      }) {
+        transform: ${this.getTransformStyle(this.radius, singleAngle * index)};
+      }
+      `;
+    });
+
+    const stylesheet = new CSSStyleSheet();
+    stylesheet.replaceSync(styles);
+    this.shadowDOM.adoptedStyleSheets.push(stylesheet);
+  }
+
+  private getTransformStyle(radius: number, angle: number): string {
+    return `rotate(${angle}deg) translate(${radius}rem) rotate(${-angle}deg)`;
+  }
+
+  onMenuToggleClick() {
+    this.getElement('continentsMenuOptions')?.classList.toggle('active');
+  }
+}
 
 registerComponent(ContinentsMenu);

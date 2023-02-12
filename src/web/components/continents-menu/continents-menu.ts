30b1dc8d625ec7294a3908e9c6040a92a61e131d
Fawad Ali <m.fawaadali98@gmail.com>
2023-02-12 19:51:24 +0100

    Enable toggling of continents menu and improve styles

--- src/web/components/continents-menu/continents-menu.ts
+++ src/web/components/continents-menu/continents-menu.ts
@@ -11,6 +11,20 @@ class ContinentsMenu extends Component {
   private rotationAngle = 90;
 
   protected onInit() {
+    const styles = this.constructMenuOptionsStyles();
+    const stylesheet = new CSSStyleSheet();
+
+    stylesheet.replaceSync(styles);
+    this.shadowDOM.adoptedStyleSheets.push(stylesheet);
+  }
+
+  onMenuToggleClick() {
+    this.hasAttribute('open')
+      ? this.removeAttribute('open')
+      : this.setAttribute('open', '');
+  }
+
+  private constructMenuOptionsStyles() {
     const menuOptions = [
       ...(this.getElement('continentsMenuOptions')?.children ?? []),
     ] as HTMLElement[];
@@ -18,28 +32,31 @@ class ContinentsMenu extends Component {
     const menuOptionsCount = menuOptions.length - 1;
     const singleAngle = this.startAngle + this.rotationAngle / menuOptionsCount;
 
-    let styles = '';
+    const styles = menuOptions.reduce((accumulated, _current, index) => {
+      const selector = this.getMenuItemSelector(index);
 
-    menuOptions.forEach((_menuOption, index) => {
-      styles += `.continents-menu-options.active > mp-circle-button:nth-child(${
-        index + 1
-      }) {
-        transform: ${this.getTransformStyle(this.radius, singleAngle * index)};
+      accumulated += `${selector} {
+        transform: ${this.getMenuItemTransformStyle(
+          this.radius,
+          singleAngle * index,
+        )};
       }
       `;
-    });
 
-    const stylesheet = new CSSStyleSheet();
-    stylesheet.replaceSync(styles);
-    this.shadowDOM.adoptedStyleSheets.push(stylesheet);
+      return accumulated;
+    }, '');
+
+    return styles;
   }
 
-  private getTransformStyle(radius: number, angle: number): string {
+  private getMenuItemTransformStyle(radius: number, angle: number): string {
     return `rotate(${angle}deg) translate(${radius}rem) rotate(${-angle}deg)`;
   }
 
-  onMenuToggleClick() {
-    this.getElement('continentsMenuOptions')?.classList.toggle('active');
+  private getMenuItemSelector(index: number) {
+    return `:host([open]) > .continents-menu-options > mp-circle-button:nth-child(${
+      index + 1
+    })`;
   }
 }
 

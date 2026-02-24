57d349cf4f4c05bc5a7ba84309ed7ea463c67322
9inpachi <9inpachi@users.noreply.github.com>
2026-02-24 19:22:02 +0000

    deploy: 58f9e925c09c9815cb6a6095c398314f4ee62795

---
+++ assets/compass-CrQWVvdj.js
@@ -0,0 +1,7 @@
+const l=`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+  <path
+    fill="currentColor"
+    d="m2.06,21.95l13.66,-6.21l6.21,-13.66l-13.66,6.21l-6.21,13.66zm2.5,-2.5l4.1,-9.01l4.92,4.92l-9.01,4.1z"
+  />
+</svg>
+`;export{l as default};
